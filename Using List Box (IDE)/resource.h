48cedb0c53c262587ca666720fc61afb95ca26e8
dimabalawov <dimabalawov@gmail.com>
2024-02-18 23:54:58 +0200

    Добавьте файлы проекта.

---
+++ Using List Box (IDE)/resource.h
@@ -0,0 +1,30 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется Using List Box (IDE).rc
+//
+#define IDD_DIALOG1                     101
+#define IDC_LIST1                       1001
+#define IDC_LIST2                       1002
+#define IDC_ADD                         1003
+#define IDC_EDIT1                       1004
+#define IDC_DEL                         1005
+#define IDC_FIND                        1006
+#define IDC_EDIT2                       1007
+#define IDC_DELALL                      1008
+#define IDC_BUTTON1                     1009
+#define IDC_GETSELITEMS                 1009
+#define IDC_STATIC                      1010
+#define IDC_STATIC2                     1011
+#define IDC_DELALL2                     1012
+#define IDC_EDIT3                       1013
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
